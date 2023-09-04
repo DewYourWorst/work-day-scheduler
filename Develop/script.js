e6ccb0e84fbec4d0f932502e2a724488408d0d91
Nicolas Alonzo <nicalonzo80@yahoo.com>
2023-09-04 18:03:00 -0500

    Improved and worked on script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-const dayjs = require('dayjs')
-dayjs().format(dd/mm/yyyy)
+var container = $(".container");
+var currentHour = dayjs()
+console.log(currentHour)
+var date = $("<h1>");
+var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+date.text(dayjs().format('dddd MMMM DD, YYYY'));
+container.append(date);
+
+function siteOpened() {
+    for (i = 0; i < 9; i++) {
+        if (i < currentHour) {
+            var timeClass = "past";
+        }
+        else if (i === currentHour) {
+            var timeClass = "present";
+        }
+        else if (i > currentHour) {
+            var timeClass = "future";
+        }
+        var row = $("<div>");
+            row.attr("class", "row");
+            container.append(row);
+        var label = $("<label>");
+            label.attr("class", "col-2 col-sm-1 time-block hour");
+            label.text(time[i]);
+            row.append(label);
+        var textArea = $("<textarea>");
+            textArea.attr("class", "col-8 col-sm-10 description " + timeClass);
+            textArea.text(localStorage.getItem("btn" + i));
+            row.append(textArea);
+        var button = $("<button>");
+            button.attr("class", "col-2 col-sm-1 saveBtn fas fa-save");
+            button.attr("id", "btn" + i);
+            row.append(button);
+    };
+    console.log(currentHour);
+    console.log(localStorage);
+};
+ window.onload = siteOpened();
+
+function saveNote(){
+  localStorage.setItem($(this).attr("id"), $(this).prev().val());
+  console.log(localStorage)
+};
+
+$(".saveBtn").on("click", saveNote);
 
 
-$(function () {
-  
-});
 
-function save(event) {
-  event.preventDefault();
 
-  console.log("Action saved at", textInputEl.val())
-  //Use this to push to Local storage
-  $.each(checkedEl, function () {
-    selected.push($(this).val());
-  });
-}
-
-saveBtn.on('submit', save)
